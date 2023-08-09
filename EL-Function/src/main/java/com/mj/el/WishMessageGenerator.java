c859a727c1de81932e2cb0e41181fe1d6fb08af3
Manoj Kumar Jena <132927540+manojkumarjena2022@users.noreply.github.com>
2023-08-09 23:21:06 +0530

    Add files via upload

---
+++ EL-Function/src/main/java/com/mj/el/WishMessageGenerator.java
@@ -0,0 +1,8 @@
+package com.mj.el;
+
+public class WishMessageGenerator {
+	public static String generate(String user)
+	{
+		return "Welcome "+user;
+	}
+}
---
+++ EL-Function/src/main/webapp/el_test.jsp
@@ -0,0 +1,2 @@
+<%@ taglib uri="http://manoj.com/mjtags" prefix="mj" %>
+Wish Message is::${mj:show('Manoj')}
